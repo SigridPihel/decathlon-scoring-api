87648d749179a670af5692154f8129eda827dda2
sipihe <sipihe@taltech.ee>
2026-07-17 17:14:41 +0300

    created spring boot project

---
+++ backend/src/main/java/com/example/decathlonscoring/DecathlonScoringApplication.java
@@ -0,0 +1,13 @@
+package com.example.decathlonscoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecathlonScoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DecathlonScoringApplication.class, args);
+    }
+
+}
