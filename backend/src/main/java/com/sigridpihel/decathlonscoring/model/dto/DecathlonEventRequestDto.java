cc3601407f6ec7ff426921366ce57a9adb91e664
sipihe <sipihe@taltech.ee>
2026-07-18 14:40:05 +0300

    added request dto

---
+++ backend/src/main/java/com/sigridpihel/decathlonscoring/model/dto/DecathlonEventRequestDto.java
@@ -0,0 +1,15 @@
+package com.sigridpihel.decathlonscoring.model.dto;
+
+import com.sigridpihel.decathlonscoring.model.enumeration.DecathlonEvent;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Positive;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public record DecathlonEventRequestDto(@NotBlank String athleteName,
+                                       @NotNull DecathlonEvent event,
+                                       @Positive @NotNull BigDecimal perfomanceValue,
+                                       @NotNull @PastOrPresent LocalDate resultDate) {}
